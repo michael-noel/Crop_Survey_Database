9fefab2bf78d89a4b5da20f1ddf5bce1a8fe8bc5
Adam H. Sparks <adamhsparks@gmail.com>
2016-07-21 19:37:26 +1000

    Add names to CSV files
    Check if first time through loop, if yes, add headers to columns.

--- Crop_Survey_DB_ADN_Form2_Visit1.R
+++ Crop_Survey_DB_ADN_Form2_Visit1.R
@@ -38,95 +38,117 @@ foreach(f = itw, .packages = c("XLConnect", "iterators", "foreach",
 
                                  extracted <- extract(f)
 
-
                                  # Join the five types of injuries from each
                                  # location into five respective data frames
                                  # from a list of data frames for each injury
                                  itx <- iter(1:5)
-
-                                 injuries <- foreach(v = itx,
-                                                     .packages =
-                                                       c("plyr", "iotools"))
-
-                                 %do% {injuries[[v]] <- join_all(extracted[[v]],
-                                                                  type = "full")
+                                 injuries <- foreach(v = itx, .packages = c("plyr", "iotools")) %do% {
+                                   injuries[[v]] <- join_all(extracted[[v]], type = "full")
                                  }
 
-                                 # add names to columns ------------------------
+                                 if (f == dsn_raw[1]) {
+                                   # add names to columns ------------------------
 
-                                 names(injuries[[1]]) <- c("location",
-                                                           "visit_date",
-                                                           "visit_no",
-                                                           "field_no",
-                                                           "water_status",
-                                                           "crop_stage",
-                                                           "rat", "silvershoot",
-                                                           "whitehead",
-                                                           "deadheart",
-                                                           "leaffolder",
-                                                           "leaf_miner",
-                                                           "rice_hispa",
-                                                           "whorl_maggot",
-                                                           "other_defoliator",
-                                                           "bacterial_blight",
-                                                           "bacterial_leaf_streak",
-                                                           "brown_spot",
-                                                           "leaf_blast",
-                                                           "leaf_scald",
-                                                           "narrow_brown_spot",
-                                                           "red_stripe",
-                                                           "dirty_panicle",
-                                                           "false_smut",
-                                                           "neck_blast",
-                                                           "sheath_blight",
-                                                           "sheath_rot")
+                                   hq_injuries_names <- c("location",
+                                                          "visit_date",
+                                                          "visit_no",
+                                                          "field_no",
+                                                          "water_status",
+                                                          "crop_stage",
+                                                          "rat", "silvershoot",
+                                                          "whitehead",
+                                                          "deadheart",
+                                                          "leaffolder",
+                                                          "leaf_miner",
+                                                          "rice_hispa",
+                                                          "whorl_maggot",
+                                                          "other_defoliator",
+                                                          "bacterial_blight",
+                                                          "bacterial_leaf_streak",
+                                                          "brown_spot",
+                                                          "leaf_blast",
+                                                          "leaf_scald",
+                                                          "narrow_brown_spot",
+                                                          "red_stripe",
+                                                          "dirty_panicle",
+                                                          "false_smut",
+                                                          "neck_blast",
+                                                          "sheath_blight",
+                                                          "sheath_rot")
 
-                                 names(injuries[[2]]) <- c("location",
-                                                           "visit_date",
-                                                           "visit_no",
-                                                           "field_no",
-                                                           "water_status",
-                                                           "crop_stage",
-                                                           "tillers",
-                                                           "leaves",
-                                                           "panicles",
-                                                           "hill_quadrat",
-                                                           "weed_area",
-                                                           "weed_above",
-                                                           "weed_below")
+                                   weed_area_names <- c("location",
+                                                        "visit_date",
+                                                        "visit_no",
+                                                        "field_no",
+                                                        "water_status",
+                                                        "crop_stage",
+                                                        "tillers",
+                                                        "leaves",
+                                                        "panicles",
+                                                        "hill_quadrat",
+                                                        "weed_area",
+                                                        "weed_above",
+                                                        "weed_below")
 
-                                 names(injuries[[3]]) <- c("location",
-                                                           "visit_date",
-                                                           "visit_no",
-                                                           "field_no",
-                                                           "water_status",
-                                                           "crop_stage",
-                                                           "broad_leaf_rank",
-                                                           "grass_rank",
-                                                           "sedge_rank",
-                                                           "small_weeds_rank")
+                                   weed_rank_names <- c("location",
+                                                        "visit_date",
+                                                        "visit_no",
+                                                        "field_no",
+                                                        "water_status",
+                                                        "crop_stage",
+                                                        "broad_leaf_rank",
+                                                        "grass_rank",
+                                                        "sedge_rank",
+                                                        "small_weeds_rank")
 
-                                 names(injuries[[4]]) <- c("location",
-                                                           "visit_date",
-                                                           "visit_no",
-                                                           "field_no",
-                                                           "water_status",
-                                                           "crop_stage",
-                                                           "spp_no.",
-                                                           "species")
+                                   weed_spp_names <- c("location",
+                                                       "visit_date",
+                                                       "visit_no",
+                                                       "field_no",
+                                                       "water_status",
+                                                       "crop_stage",
+                                                       "spp_no.",
+                                                       "species")
 
-                                 names(injuries[[5]]) <- c("location",
-                                                           "visit_date",
-                                                           "visit_no",
-                                                           "field_no",
-                                                           "water_status",
-                                                           "crop_stage",
-                                                           "grassy_stunt",
-                                                           "ragged_stunt",
-                                                           "rice_tungro",
-                                                           "yellowing_syndrome",
-                                                           "hopperburn",
-                                                           "bugburn")
+                                   systemic_injury_names <- c("location",
+                                                              "visit_date",
+                                                              "visit_no",
+                                                              "field_no",
+                                                              "water_status",
+                                                              "crop_stage",
+                                                              "grassy_stunt",
+                                                              "ragged_stunt",
+                                                              "rice_tungro",
+                                                              "yellowing_syndrome",
+                                                              "hopperburn",
+                                                              "bugburn")
+
+                                   cat(noquote(paste0(paste0(hq_injuries_names,
+                                                             collapse = ","),
+                                                      "\n")),
+                                       file = paste0(path.expand(dsn_cleaned),
+                                                     "hq_injuries.csv"))
+                                   cat(noquote(paste0(paste0(weed_area_names,
+                                                             collapse = ","),
+                                                      "\n")),
+                                       file = paste0(path.expand(dsn_cleaned),
+                                                     "weed_area.csv"))
+                                   cat(noquote(paste0(paste0(weed_rank_names,
+                                                             collapse = ","),
+                                                      "\n")),
+                                       file = paste0(path.expand(dsn_cleaned),
+                                                     "weed_rank.csv"))
+                                   cat(noquote(paste0(paste0(weed_spp_names,
+                                                             collapse = ","),
+                                                      "\n")),
+                                       file = paste0(path.expand(dsn_cleaned),
+                                                     "weed_spp.csv"))
+                                   cat(noquote(paste0(paste0(systemic_injury_names,
+                                                             collapse = ","),
+                                                      "\n")),
+                                       file = paste0(path.expand(dsn_cleaned),
+                                                     "systemic_injuries.csv"))
+                                 }
 
                                  # Write CSV files out to disk -----------------
                                  write.csv.raw(
